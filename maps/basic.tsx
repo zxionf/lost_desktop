56a490bd3d3675bcf4948048cddad0db5177c66c
zxionf <3512608310@qq.com>
2025-05-12 09:58:17 +0800

    ui

--- maps/basic.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="basic" tilewidth="16" tileheight="16" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image source="texture/blocks/furnace_top.png" width="16" height="16"/>
- </tile>
-</tileset>
