78476840e6a07811287f0d98d0365d363d5f618d
zxionf <3512608310@qq.com>
2025-05-16 16:36:30 +0800

    a

---
+++ webtranslate/src/main/java/org/example/A.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Locale;
+import java.awt.im.InputContext;
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        InputContext context = InputContext.getInstance();
+        String locale = context.getLocale().toString();
+//        if(locale.startsWith("zh")){
+            context.selectInputMethod(Locale.ENGLISH);
+            System.out.println("Locale: " + locale);
+//        }
+
+    }
+}
