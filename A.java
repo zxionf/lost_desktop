7ad58ef9b28767ee634fc4af61aed577320245d7
zxionf <3512608310@qq.com>
2025-05-27 19:59:37 +0800

    network

--- A.java
+++
@@ -1,8 +0,0 @@
-public class A {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        for(int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-    }
-}
